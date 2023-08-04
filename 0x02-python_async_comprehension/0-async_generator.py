dafc7341332dae7eb4866755f35b18e16ad8c7be
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-08-04 18:12:02 +0100

    Initial commit

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -19,4 +19,4 @@ async def async_generator() -> Generator[float, None, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
+        yield Generator[float, None, None]random.uniform(0, 10)  # Yield a random number between 0 and 10
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
-"""
-"""
+"""Parameterize a unit test """
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
+    """Test class for Access Nested Map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
+
     def test_access_nested_map(self, nested_map, path, expected_result):
         """access_nested_map()  test method"""
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
   
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path):
+        """access_nested_map exception test"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Check that the exception message is as expected
+        self.assertEqual(
+            str(context.exception),
+            f"Key not found: {path[-1]}"
+        )
