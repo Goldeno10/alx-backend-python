225ee976aff73f36ac72ba8136cc16c082d7d158
Goldneo10 <ibrahimmuhammad271@gmail.com>
2023-08-06 20:26:08 +0100

    client test initial commit

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -40,3 +40,23 @@ class TestGithubOrgClient(unittest.TestCase):
                 "repos_url": f"https://api.github.com/orgs/{org_name}/repos"
             }
         )
+    
+    @patch('client.get_json')
+    def test_public_repos_url(self, mock_get_json):
+        """public_repos_url() test method"""
+        org_name = "testorg"
+        repos_url = "https://api.github.com/orgs/testorg/repos"
+        # Mock the get_json response for org method
+        mock_get_json.return_value = {"name": org_name, "repos_url": repos_url}
+
+        # Create an instance of GithubOrgClient
+        client = GithubOrgClient(org_name)
+
+        # Access the _public_repos_url property
+        result = client._public_repos_url
+
+        # Assert that the mocked get_json is called once with the expected argument
+        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+
+        # Assert that the result is correct
+        self.assertEqual(result, repos_url)
